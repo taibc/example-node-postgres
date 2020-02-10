28ffe1e517ffd10fee22e467a7fbf788c29d5f25
taibc <37021673+taibc@users.noreply.github.com>
2020-02-10 21:31:47 +0700

    Update config.js

--- src/db/config.js
+++ src/db/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   configDB: {
     user: "userBLC",
-    host: "172.17.0.9",
+    host: "172.30.225.47",
     database: "sampledb",
     password: "KqaEB8wT4UUur3vB",
     port: 5432
